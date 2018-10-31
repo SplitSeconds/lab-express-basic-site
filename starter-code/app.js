18a7d5dec0474bee008283e1600fd2599255549f
Nele Luepkes <nluepkes@gmail.com>
2018-10-31 15:09:29 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+// Route
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/home', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/photos', (request, response, next) => {
+  response.sendFile(__dirname + '/views/photos.html');
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
